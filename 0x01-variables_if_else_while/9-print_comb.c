3999d0d79f6c3a201a3a4060f48a70c3511f116c
ollie1226 <1226@holbertonschool.com>
2019-09-23 07:11:01 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * Printed in ascending order
+ * Start from 0
+ * Only use 4xputchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+		if (i == 57)
+                {
+                        break;
+                }
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
